84b5d7f8b8b0e8836e4dc0ee98247467540e1645
utilrr <gmarkghealy@gmail.com>
2022-05-12 16:15:14 -0400

    another edit

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -4,14 +4,14 @@ const Engineer = require("../lib/Engineer");
 
 //checks to see that the engineers role returns the correct answer
 test("check engineer role", () => {
-  const engineer = new Engineer("Blair", "1234", "email@gmail.com", "dogmom3");
+  const engineer = new Engineer("BOB", "1234", "email@gmail.com", "dogmom3");
   expect(engineer.getRole()).toBe("Engineer");
 });
 
 //checks to see if all other engineer data returns correctly
 test("engineer class", () => {
-  const engineer = new Engineer("Blair", "1234", "email@gmail.com", "dogmom3");
-  expect(engineer.name).toBe("Blair");
+  const engineer = new Engineer("BOB", "1234", "email@gmail.com", "dogmom3");
+  expect(engineer.name).toBe("BOB");
   expect(engineer.id).toBe("1234");
   expect(engineer.email).toBe("email@gmail.com");
   expect(engineer.gitHub).toBe("dogmom3");
